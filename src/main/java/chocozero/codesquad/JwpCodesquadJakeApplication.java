07d39971d81a71595f56ac911d241564d11175ff
chocozero <chocozero@218.38.137.28>
2017-07-31 16:28:33 +0900

    initial java web project

---
+++ src/main/java/chocozero/codesquad/JwpCodesquadJakeApplication.java
@@ -0,0 +1,12 @@
+package chocozero.codesquad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwpCodesquadJakeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JwpCodesquadJakeApplication.class, args);
+	}
+}
