04cf0ffb163b70dc8f3f4d64e68f467e24bfe6ab
chocozero <chocozero@218.38.137.28>
2017-08-02 21:26:37 +0900

    질문목록 구현(기존 질문하기 경로 수정 포), 질문상세 보기 구현

--- src/main/java/chocozero/codesquad/web/QuestionController.java
+++ src/main/java/chocozero/codesquad/web/QuestionController.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -16,13 +17,21 @@ public class QuestionController {
 	@PostMapping("/qna")
 	ModelAndView ask(Question question) {
 		questions.add(question);
-		return new ModelAndView("redirect:/qna");
+		return new ModelAndView("redirect:/");
 	}
 	
-	@GetMapping("/qna")
-	ModelAndView showQuestions() {
-		ModelAndView mav = new ModelAndView("qna/show");
+	@GetMapping("/")
+	ModelAndView getQuestions() {
+		ModelAndView mav = new ModelAndView("qna/list");
 		mav.addObject("questions", questions);
+		return mav;
+	}
+	
+	@GetMapping("/questions/{index}")
+	ModelAndView showQuestionDetail(@PathVariable int index) {
+		Question question = questions.get(index);
+		ModelAndView mav = new ModelAndView("qna/show");
+		mav.addObject("question", question);
 		return mav;
 	}
 }
